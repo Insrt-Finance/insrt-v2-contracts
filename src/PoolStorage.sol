8e85853a2bd3286fd516d03e558ad62b887b469e
NouDaimon <peric.iason@gmail.com>
2023-05-29 15:10:20 -0500

    feat/init storage layout

---
+++ src/PoolStorage.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.20;
+
+import { EnumerableSet } from '@solidstate-solidity/data/EnumerableSet.sol';
+
+library PoolStorage {
+    struct Layout {
+        mapping(address collection => uint256 status) whitelistedCollections;
+        mapping(address collection => EnumerableSet.UintSet tokenIds) poolAssets; //perhaps rename
+    }
+
+    bytes32 internal constant STORAGE_SLOT =
+        keccak256('insrt.contracts.storage.INLP');
+
+    function layout() internal pure returns (Layout storage l) {
+        bytes32 slot = STORAGE_SLOT;
+        assembly {
+            l.slot := slot
+        }
+    }
+}

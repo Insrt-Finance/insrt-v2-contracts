6fbb3c323d973c94783cb79f75ae0c92b9a950c9
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2023-06-26 10:48:04 -0700

    build: add setLayerZeroTrustedRemoteAddress fn selector to L2AssetHandlerHelper for diamondCut

--- test/facets/L2/AssetHandler/AssetHandlerHelper.t.sol
+++ test/facets/L2/AssetHandler/AssetHandlerHelper.t.sol
@@ -34,14 +34,17 @@ contract L2AssetHandlerHelper {
         view
         returns (ISolidStateDiamond.FacetCut[] memory)
     {
-        bytes4[] memory functionSelectors = new bytes4[](3);
+        bytes4[] memory functionSelectors = new bytes4[](4);
 
         // Map the function selectors to their respective interfaces.
         functionSelectors[0] = IAssetHandler
             .setLayerZeroChainIdDestination
             .selector;
-        functionSelectors[1] = IL2AssetHandler.unstakeERC1155Assets.selector;
-        functionSelectors[2] = IL2AssetHandler.unstakeERC721Assets.selector;
+        functionSelectors[1] = IAssetHandler
+            .setLayerZeroTrustedRemoteAddress
+            .selector;
+        functionSelectors[2] = IL2AssetHandler.unstakeERC1155Assets.selector;
+        functionSelectors[3] = IL2AssetHandler.unstakeERC721Assets.selector;
 
         ISolidStateDiamond.FacetCut memory facetCut = IDiamondWritableInternal
             .FacetCut({

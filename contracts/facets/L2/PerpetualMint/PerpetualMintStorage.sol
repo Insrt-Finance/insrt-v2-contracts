64c159c18d25dded6b4e17bb2970a26f9ec63009
NouDaimon <81045441+NouDaimon@users.noreply.github.com>
2023-07-05 15:10:14 +0300

    Upon successful mint, remove token from play (#35)
    * feat: rework ERC721 mint flow to deactive assets upon successful mint
    
    * feat: update ERC1155 mint resolving to handle active/inactive token status
    
    * fix: fix random word request, remove unecessary variables
    
    * fix: remove unnused storage variables, remove tokenId from active token Ids after successful mint
    
    * fix: remove address from escrowedTokenOwners if no tokens held
    
    * fix: rename storage variable
    
    * fix: rename storage variables, reorganize storage
    
    * refactor: move errors and events into IPerpetualMint
    
    * fix: rename IPerpetualMint => IPerpetualMintInternal

--- contracts/facets/L2/PerpetualMint/PerpetualMintStorage.sol
+++ contracts/facets/L2/PerpetualMint/PerpetualMintStorage.sol
@@ -5,29 +5,34 @@ pragma solidity ^0.8.20;
 import { EnumerableSet } from "@solidstate/contracts/data/EnumerableSet.sol";
 
 library PerpetualMintStorage {
+    //note: must add whitelist collections mapping
     struct Layout {
         uint256 protocolFees;
         uint64 id;
         uint32 mintFeeBP;
         mapping(uint256 requestId => address account) requestAccount;
         mapping(uint256 requestId => address collection) requestCollection;
+        mapping(address collection => bool status) isWhitelisted;
         mapping(address collection => bool isERC721) collectionType;
         mapping(address collection => uint256 amount) collectionEarnings;
         mapping(address collection => uint256 mintPrice) collectionMintPrice;
-        mapping(address collection => uint256 amount) totalEscrowedTokenAmount;
         mapping(address collection => uint128 risk) totalRisk;
-        mapping(address collection => EnumerableSet.UintSet tokenIds) escrowedTokenIds;
+        mapping(address collection => uint256 amount) totalActiveTokens;
+        mapping(address collection => EnumerableSet.UintSet tokenIds) activeTokenIds;
         mapping(address collection => mapping(address account => uint256 amount)) accountDeductions;
         mapping(address collection => mapping(address account => uint256 amount)) accountEarnings;
-        mapping(address collection => mapping(address account => uint256 amount)) escrowedTokenAmount;
+        mapping(address collection => mapping(address account => uint256 amount)) inactiveTokens;
+        mapping(address collection => mapping(address account => uint256 amount)) activeTokens;
         mapping(address collection => mapping(uint256 tokenId => uint256 risk)) tokenRisk;
         //ERC721
-        mapping(address collection => mapping(uint256 tokenId => address account)) escrowedERC721TokenOwner;
+        mapping(address collection => mapping(uint256 tokenId => address account)) escrowedERC721Owner;
         //ERC1155
-        mapping(address collection => mapping(uint256 tokenId => EnumerableSet.AddressSet accounts)) escrowedERC1155TokenOwners;
+        mapping(address collection => mapping(uint256 tokenId => EnumerableSet.AddressSet accounts)) escrowedERC1155Owners;
         mapping(address collection => mapping(uint256 tokenId => mapping(address account => uint64 risk))) accountTokenRisk;
-        mapping(address collection => mapping(uint256 tokenId => uint64 risk)) totalERC1155TokenRisk;
-        mapping(address collection => mapping(uint256 tokenId => mapping(address account => uint256 amount))) escrowedERC1155TokenAmount;
+        mapping(address collection => mapping(uint256 tokenId => uint64 risk)) totalTokenRisk;
+        mapping(address collection => mapping(uint256 tokenId => EnumerableSet.AddressSet accounts)) activeERC1155Owners;
+        mapping(address collection => mapping(uint256 tokenId => mapping(address account => uint256 amount))) activeERC1155Tokens;
+        mapping(address collection => mapping(uint256 tokenId => mapping(address account => uint256 amount))) inactiveERC1155Tokens;
     }
 
     bytes32 internal constant STORAGE_SLOT =

50ee9e6e1acd3de6267e50878ab6d1c89eb944c2
NouDaimon <peric.iason@gmail.com>
2023-07-06 14:13:10 +0300

    eat: remove whitelist, increase BASIS

--- contracts/facets/L2/PerpetualMint/PerpetualMintStorage.sol
+++ contracts/facets/L2/PerpetualMint/PerpetualMintStorage.sol
@@ -11,7 +11,6 @@ library PerpetualMintStorage {
         uint32 mintFeeBP;
         mapping(uint256 requestId => address account) requestAccount;
         mapping(uint256 requestId => address collection) requestCollection;
-        mapping(address collection => bool status) isWhitelisted; // is this needed?
         mapping(address collection => bool isERC721) collectionType;
         mapping(address collection => uint256 amount) collectionEarnings;
         mapping(address collection => uint256 mintPrice) collectionMintPrice;

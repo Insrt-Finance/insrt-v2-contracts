a1d21af22199c5d0b7c8bd6b62eba7676e702eed
NouDaimon <peric.iason@gmail.com>
2023-08-24 15:44:52 +0300

    feat: add _enforceNoUnfulfilledRequests guard

--- contracts/facets/L2/common/GuardsInternal.sol
+++ contracts/facets/L2/common/GuardsInternal.sol
@@ -2,12 +2,15 @@
 
 pragma solidity ^0.8.21;
 
+import { EnumerableSet } from "@solidstate/contracts/data/EnumerableSet.sol";
 import { IGuardsInternal } from "./IGuardsInternal.sol";
 import { PerpetualMintStorage as Storage } from "../PerpetualMint/Storage.sol";
 
 /// @title Guards contract
 /// @dev contains guard function implementation and setters of related variables
 abstract contract GuardsInternal is IGuardsInternal {
+    using EnumerableSet for EnumerableSet.UintSet;
+
     /// @dev enforces the maximum active token limit on an amount of tokens
     /// @param l the PerpetualMint storage layout
     /// @param tokens amount to check

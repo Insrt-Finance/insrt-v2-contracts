f391275fb6d6f350c69d76bb6cd86ecf0ae856c7
NouDaimon <peric.iason@gmail.com>
2023-09-05 03:07:40 +0300

    feat: TokenProxy contract, ITokenProxy interface

---
+++ contracts/diamonds/token/TokenProxy.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.21;
+
+import { SolidStateDiamond } from "@solidstate/contracts/proxy/diamond/SolidStateDiamond.sol";
+
+/// @title TokenProxy
+/// @dev The TokenProxy Diamond.
+contract TokenProxy is SolidStateDiamond {
+
+}

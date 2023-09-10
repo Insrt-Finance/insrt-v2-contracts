b7f72bdb61c5c31a06497aab5b6844359f732072
NouDaimon <peric.iason@gmail.com>
2023-09-10 09:31:06 +0300

    build: update mint tests

--- test/facets/token/behaviors/mint.t.sol
+++ test/facets/token/behaviors/mint.t.sol
@@ -5,6 +5,8 @@ pragma solidity 0.8.21;
 import { TokenTest } from "../Token.t.sol";
 import { ArbForkTest } from "../../../ArbForkTest.t.sol";
 
+import "forge-std/console.sol";
+
 /// @title Token_mint
 /// @dev Token test contract for testing expected mint behavior. Tested on an Arbitrum fork.
 contract Token_mint is ArbForkTest, TokenTest {
@@ -27,6 +29,7 @@ contract Token_mint is ArbForkTest, TokenTest {
         uint256 expectedRatio;
         assert(globalRatio == expectedRatio);
 
+        uint256 userBalance = token.balanceOf(MINTER);
         vm.prank(MINTER);
         token.mint(MINTER, MINT_AMOUNT);
 
@@ -34,12 +37,15 @@ contract Token_mint is ArbForkTest, TokenTest {
 
         expectedRatio +=
             (DISTRIBUTION_AMOUNT * SCALE) /
-            (MINT_AMOUNT - DISTRIBUTION_AMOUNT);
+            (MINT_AMOUNT - DISTRIBUTION_AMOUNT - userBalance);
+
         assert(globalRatio == expectedRatio);
+
+        userBalance = token.balanceOf(RECEIVER);
 
         expectedRatio +=
             (DISTRIBUTION_AMOUNT * SCALE) /
-            (token.totalSupply() - token.distributionSupply());
+            (token.totalSupply() - token.distributionSupply() - userBalance);
 
         vm.prank(MINTER);
         token.mint(RECEIVER, MINT_AMOUNT);
@@ -69,9 +75,36 @@ contract Token_mint is ArbForkTest, TokenTest {
         assert(globalRatio == accountOffset);
     }
 
-    /// @dev ensures that mint, when there is 1 token holder, updates the global ratio based on
-    /// the amount being minted
-    function test_mintUpdatesGlobalRatioByDistributionAmountOverMintAmountIfFirstMintOrOnleSingleTokenHolderMinting()
+    /// @dev ensures that mint, when there are more than 1 token holders, updates the accruedTokens of the account receiving the
+    /// minted tokens
+    function test_mintIncreasesAccountAccruedTokensWhenMoreThanOneTokenHolder()
+        public
+    {
+        vm.prank(MINTER);
+        token.mint(MINTER, MINT_AMOUNT);
+
+        vm.prank(MINTER);
+        token.mint(RECEIVER, MINT_AMOUNT);
+
+        uint256 oldAccruedTokens = token.accruedTokens(MINTER);
+
+        vm.prank(MINTER);
+        token.mint(MINTER, MINT_AMOUNT);
+
+        uint256 newAccruedTokens = token.accruedTokens(MINTER);
+        uint256 expectedAccruedTokens = 2 * DISTRIBUTION_AMOUNT;
+
+        assert(
+            newAccruedTokens - oldAccruedTokens + 1 >= expectedAccruedTokens
+        );
+        assert(
+            newAccruedTokens - oldAccruedTokens - 1 <= expectedAccruedTokens
+        );
+    }
+
+    /// @dev ensures that mint, when the only token holder is the first minter, updates
+    /// the global ratio based on the amount being minted
+    function test_mintUpdatesGlobalRatioByDistributionRatioIfOnlyFirstMinterMinting()
         public
     {
         uint256 globalRatio = token.globalRatio();
@@ -100,6 +133,166 @@ contract Token_mint is ArbForkTest, TokenTest {
                     (MINT_AMOUNT - DISTRIBUTION_AMOUNT)) *
                     2
         );
+    }
+
+    /// @dev ensures mint, when the only token holder is the first minter, sets the account offset of the minter to
+    /// the globalRatio minus the distributionRatio
+    function test_mintUpdatesAccountOffsetToGlobalRatioMinusDistributionRatioIfOnlyFirstMinterMinting()
+        public
+    {
+        uint256 globalRatio = token.globalRatio();
+        uint256 distributionRatio = (SCALE * DISTRIBUTION_AMOUNT) /
+            (MINT_AMOUNT - DISTRIBUTION_AMOUNT);
+
+        assert(globalRatio == 0);
+
+        vm.prank(MINTER);
+        token.mint(MINTER, MINT_AMOUNT);
+
+        globalRatio = token.globalRatio();
+
+        assert(
+            globalRatio ==
+                (DISTRIBUTION_AMOUNT * SCALE) /
+                    (MINT_AMOUNT - DISTRIBUTION_AMOUNT)
+        );
+
+        vm.prank(MINTER);
+        token.mint(MINTER, MINT_AMOUNT);
+
+        globalRatio = token.globalRatio();
+
+        assert(token.accountOffset(MINTER) == globalRatio - distributionRatio);
+    }
+
+    /// @dev ensures mint, when there is a single token holder who is _not_ the first mint,
+    /// increases accrued tokens of the account minting
+    function test_mintIncreasesAccruedTokensOfAccountByDistributionAmountAndPreviousAccrualsWhenOnlySingleTokenHolder()
+        public
+    {
+        uint256 globalRatio = token.globalRatio();
+
+        assert(globalRatio == 0);
+
+        vm.prank(MINTER);
+        token.mint(MINTER, MINT_AMOUNT);
+
+        globalRatio = token.globalRatio();
+
+        assert(
+            globalRatio ==
+                (DISTRIBUTION_AMOUNT * SCALE) /
+                    (MINT_AMOUNT - DISTRIBUTION_AMOUNT)
+        );
+
+        vm.prank(MINTER);
+        token.mint(RECEIVER, MINT_AMOUNT);
+
+        // at this point RECEIVER is entitled to DISTRIBUTION_AMOUNT of tokens
+        // since they are the only other token holder
+        vm.startPrank(MINTER);
+        token.mint(MINTER, MINT_AMOUNT);
+
+        token.mint(RECEIVER, MINT_AMOUNT);
+
+        // remove all of MINTER's tokens
+        token.claim();
+
+        token.burn(MINTER, token.balanceOf(MINTER));
+
+        token.mint(RECEIVER, MINT_AMOUNT);
+
+        // RECEIVER is now entitled to 2 * DISTRIBUTION_AMOUNT of tokens
+        // since they are entitled to the full DISTRIBUTION_AMOUNT they
+        // contributed
+        assert(2 * DISTRIBUTION_AMOUNT + 1 >= token.accruedTokens(RECEIVER));
+        assert(2 * DISTRIBUTION_AMOUNT - 1 <= token.accruedTokens(RECEIVER));
+    }
+
+    /// @dev ensures mint, when there is a single token holder who is _not_ the first mint,
+    /// increases globalRatio by the distributionRatio
+    function test_mintIncreasesGlobalRatioByDistributionRatioWhenOnlySingleTokenHolder()
+        public
+    {
+        uint256 globalRatio = token.globalRatio();
+        uint256 distributionRatio = (DISTRIBUTION_AMOUNT * SCALE) /
+            (MINT_AMOUNT - DISTRIBUTION_AMOUNT);
+
+        assert(globalRatio == 0);
+
+        vm.prank(MINTER);
+        token.mint(MINTER, MINT_AMOUNT);
+
+        globalRatio = token.globalRatio();
+
+        assert(
+            globalRatio ==
+                (DISTRIBUTION_AMOUNT * SCALE) /
+                    (MINT_AMOUNT - DISTRIBUTION_AMOUNT)
+        );
+
+        vm.prank(MINTER);
+        token.mint(RECEIVER, MINT_AMOUNT);
+
+        // at this point RECEIVER is entitled to DISTRIBUTION_AMOUNT of tokens
+        // since they are the only other token holder
+        vm.startPrank(MINTER);
+        token.mint(MINTER, MINT_AMOUNT);
+
+        token.mint(RECEIVER, MINT_AMOUNT);
+
+        // remove all of MINTER's tokens
+        token.claim();
+
+        token.burn(MINTER, token.balanceOf(MINTER));
+
+        uint256 oldGlobalRatio = token.globalRatio();
+
+        token.mint(RECEIVER, MINT_AMOUNT);
+
+        uint256 newGlobalRatio = token.globalRatio();
+
+        assert(newGlobalRatio - oldGlobalRatio == distributionRatio);
+    }
+
+    /// @dev ensures mint, when there is a single token holder who is _not_ the first mint,
+    /// sets accountOffset of minter to globalRatio
+    function test_mintUpdatesAccountOffsetOfToGlobalRatioWhenOnlySingleTokenHolder()
+        public
+    {
+        uint256 globalRatio = token.globalRatio();
+
+        assert(globalRatio == 0);
+
+        vm.prank(MINTER);
+        token.mint(MINTER, MINT_AMOUNT);
+
+        globalRatio = token.globalRatio();
+
+        assert(
+            globalRatio ==
+                (DISTRIBUTION_AMOUNT * SCALE) /
+                    (MINT_AMOUNT - DISTRIBUTION_AMOUNT)
+        );
+
+        vm.prank(MINTER);
+        token.mint(RECEIVER, MINT_AMOUNT);
+
+        // at this point RECEIVER is entitled to DISTRIBUTION_AMOUNT of tokens
+        // since they are the only other token holder
+        vm.startPrank(MINTER);
+        token.mint(MINTER, MINT_AMOUNT);
+
+        token.mint(RECEIVER, MINT_AMOUNT);
+
+        // remove all of MINTER's tokens
+        token.claim();
+
+        token.burn(MINTER, token.balanceOf(MINTER));
+
+        token.mint(RECEIVER, MINT_AMOUNT);
+
+        assert(token.accountOffset(RECEIVER) == token.globalRatio());
     }
 
     /// @dev ensures that mint increases the distribution supply by the distribution amount

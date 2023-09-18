3c6af99a81c8fb7815bf931e54d257150423a580
NouDaimon <peric.iason@gmail.com>
2023-09-19 00:09:09 +0300

    build: update burnReceipt test

--- test/facets/PerpetualMint/behaviors/burnReceipt.t.sol
+++ test/facets/PerpetualMint/behaviors/burnReceipt.t.sol
@@ -10,18 +10,38 @@ import { ArbForkTest } from "../../../ArbForkTest.t.sol";
 /// @dev PerpetualMint_burnReceipt
 /// @dev PerpetualMint test contract for testing expected behavior of the burnReceipt function
 contract PerpetualMint_burnReceipt is ArbForkTest, PerpetualMintTest {
-    uint256 internal constant RECEIPT_ID = 123123123;
+    /// @dev test collection prize address
+    address internal testCollection = address(0xdeadbeef);
+
+    /// @dev test collection prize address encoded as uint256
+    uint256 internal testTokenId = uint256(bytes32(abi.encode(testCollection)));
 
     /// @dev sets up the context for the test cases
     function setUp() public override {
         super.setUp();
 
-        //TODO: mint test receipt
+        vm.prank(minter);
+        perpetualMint.mintReceipts(testCollection, 1);
+
+        vm.prank(minter);
+        perpetualMint.claimPrize(minter, testTokenId);
     }
 
     /// @dev tests that burnReceipt burns a single receipt of the given token ID from the PerpetualMint contract
     function test_burnReceiptBurnsSingleReceiptOfTokenId() external {
-        //TODO
+        uint256 oldBalance = perpetualMint.exposed_balanceOf(
+            address(perpetualMint),
+            testTokenId
+        );
+
+        perpetualMint.burnReceipt(testTokenId);
+
+        uint256 newBalance = perpetualMint.exposed_balanceOf(
+            address(perpetualMint),
+            testTokenId
+        );
+
+        assert(oldBalance - newBalance == 1);
     }
 
     /// @dev tests that burnReceipt will revert if called by non-owner
@@ -29,6 +49,6 @@ contract PerpetualMint_burnReceipt is ArbForkTest, PerpetualMintTest {
         vm.expectRevert(IOwnableInternal.Ownable__NotOwner.selector);
 
         vm.prank(PERPETUAL_MINT_NON_OWNER);
-        perpetualMint.burnReceipt(RECEIPT_ID);
+        perpetualMint.burnReceipt(testTokenId);
     }
 }
